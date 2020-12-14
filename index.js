c2c671b64ef5ff8f9542dab368a3ef1a62a97e0d
leeya018 <leeyahav018@gmail.com>
2020-12-14 18:17:54 +0200

    update server for heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path');
 
 const emailRouter = require('./server/routes/email-router')
 const userRouter = require('./server/routes/user-router')
@@ -12,6 +13,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
 
+app.use(express.static(path.join(__dirname,'client/build')))
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
@@ -19,6 +22,8 @@ app.get('/', (req, res) => {
 app.use('/api', emailRouter)
 app.use('/api', userRouter)
 
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'client/build/index.html'))
+})
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
